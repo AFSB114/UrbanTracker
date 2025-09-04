9c4a086bb8fdd7239d5a477fe284fa16f1309795
Jurassickk <cuellardiego310@gmail.com>
2025-09-04 17:19:17 -0500

    feat(backend): add general factory methods in vehicle

--- Backend/src/main/java/com/sena/urbantracker/vehicles/service/VehicleService.java
+++ Backend/src/main/java/com/sena/urbantracker/vehicles/service/VehicleService.java
@@ -1,30 +1,87 @@
 package com.sena.urbantracker.vehicles.service;
 
-import com.sena.urbantracker.vehicles.model.dto.request.PaginateVehicleCountsDTO;
-import com.sena.urbantracker.vehicles.model.dto.response.PaginatedVehicleResponseDTO;
-import com.sena.urbantracker.vehicles.model.entity.Vehicle;
-import com.sena.urbantracker.vehicles.repository.IVehicle;
+import com.sena.urbantracker.shared.model.dto.CrudResponseDto;
+import com.sena.urbantracker.shared.repository.ActivableEntity;
+import com.sena.urbantracker.shared.repository.CrudOperations;
+import com.sena.urbantracker.vehicles.model.dto.response.VehicleDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class VehicleService {
+public class VehicleService implements CrudOperations<VehicleDto, Long>, ActivableEntity<VehicleDto> {
 
-    private final IVehicle iVehicle;
 
-    public List<Vehicle> getAllVehicles() {
-        return iVehicle.findAll();
+    @Override
+    public CrudResponseDto<VehicleDto> activate() {
+        return new CrudResponseDto<>();
     }
 
-    public Vehicle getVehicleById(Long id) {
-        return iVehicle.findById(id).get();
+    @Override
+    public CrudResponseDto<VehicleDto> deactivate() {
+        return null;
     }
 
+    @Override
+    public CrudResponseDto<Boolean> isActive() {
+        return null;
+    }
+
+    @Override
+    public CrudResponseDto<Boolean> isNotActive() {
+        return null;
+    }
+
+    @Override
+    public CrudResponseDto<List<VehicleDto>> findAllActive() {
+        return null;
+    }
+
+    @Override
+    public CrudResponseDto<List<VehicleDto>> findAllInactive() {
+        return null;
+    }
+
+    @Override
+    public CrudResponseDto<VehicleDto> create(VehicleDto entity) {
+        return null;
+    }
+
+    @Override
+    public CrudResponseDto<Optional<VehicleDto>> findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public CrudResponseDto<List<VehicleDto>> findAll() {
+        return null;
+    }
+
+    @Override
+    public CrudResponseDto<VehicleDto> update(VehicleDto entity) {
+        return null;
+    }
+
+    @Override
+    public CrudResponseDto<Void> deleteById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public CrudResponseDto<VehicleDto> activateById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public CrudResponseDto<VehicleDto> deactivateById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public CrudResponseDto<Boolean> existsById(Long aLong) {
+        return null;
+    }
 }
