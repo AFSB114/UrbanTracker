961cd1855de640969d83a1ed35c18eb8934ecb12
carlosJavierRodriguez <carlosjavi1887@gmail.com>
2025-09-15 10:18:17 -0500

    feat(init): enhance data initialization to create default roles and admin user

--- Backend/src/main/java/com/sena/urbantracker/config/init/DataInitializer.java
+++ Backend/src/main/java/com/sena/urbantracker/config/init/DataInitializer.java
@@ -2,37 +2,74 @@ package com.sena.urbantracker.config.init;
 
 
 import com.sena.urbantracker.security.model.entity.Role;
+import com.sena.urbantracker.security.model.entity.User;
 import com.sena.urbantracker.security.repository.IRole;
+import com.sena.urbantracker.security.repository.IUser;
+import com.sena.urbantracker.users.model.entity.UserProfile;
+import com.sena.urbantracker.users.repository.IUserProfile;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+@AllArgsConstructor
 @Component
 public class DataInitializer implements CommandLineRunner {
 
     private final IRole Irole;
+    private final IUser Iuser;
+    private final PasswordEncoder passwordEncoder;
+    private final IUserProfile IuserProfile;
 
-    public DataInitializer(IRole roleRepository) {
-        this.Irole = roleRepository;
-    }
-
-    //Creo los roles al iniciar el proyecto
+    // Creo los roles al iniciar el proyecto
     @Override
     public void run(String... args) {
 
-        //verifico si los roles existen para no crearlos de nuevo
-        if (Irole.findByName("ROLE_ADMIN").isEmpty()) {
-            Role admin = Role.builder()
-                    .name("ROLE_ADMIN")
-                    .description("Tiene acceso completo al sistema")
-                    .build();
-            Irole.save(admin);
-        }
-        if (Irole.findByName("ROLE_DRIVER").isEmpty()) {
-            Role driver = Role.builder()
-                    .name("ROLE_DRIVER")
-                    .description("Usuario con permisos limitados a las funcionalidades de conductor")
-                    .build();
-            Irole.save(driver);
+        // Verifico si el rol ADMIN existe en la base de datos,
+        // si no existe, lo creo y lo guardo
+        Role adminRole = Irole.findByName("ROLE_ADMIN")
+                .orElseGet(() -> Irole.save(
+                        Role.builder()
+                                .name("ROLE_ADMIN")
+                                .description("Tiene acceso completo al sistema")
+                                .build()
+                ));
+
+        // Verifico si el rol DRIVER existe en la base de datos,
+        // si no existe, lo creo y lo guardo
+        Irole.findByName("ROLE_DRIVER")
+                .orElseGet(() -> Irole.save(
+                        Role.builder()
+                                .name("ROLE_DRIVER")
+                                .description("Usuario con permisos limitados a las funcionalidades de conductor")
+                                .build()
+                ));
+
+        // Crear usuario ADMIN si no existe
+        String adminUsername = "admin";
+        // Si no existe un usuario con username = "admin" creo uno
+        if (Iuser.findByUserName(adminUsername).isEmpty()) {
+
+            //Datos de User
+            User adminUser = new User();
+            adminUser.setUserName(adminUsername);
+            adminUser.setPassword(passwordEncoder.encode("admin123"));
+            // le asigno el rol ADMIN creado arriba
+            adminUser.setRole(adminRole);
+
+            //Datos de UserProfile
+            UserProfile adminUserProfile = new UserProfile();
+            adminUserProfile.setFirstName("Super");
+            adminUserProfile.setLastName("Admin");
+            adminUserProfile.setEmail("urbantracker751@gmail.com");
+            // le asigno el User admin creado arriba
+            adminUserProfile.setUser(adminUser);
+
+            Iuser.save(adminUser);
+            IuserProfile.save(adminUserProfile);
+
+            System.out.println("✅ Usuario ADMIN creado (user:" + adminUsername + "/ pass: admin123)");
         }
     }
+
 }
