d731620ccb1ffe2c66edace2135850c068b2c94f
AFSB114 <asuazab13@gmail.com>
2025-09-21 20:15:07 -0500

    Add authentication to swagger documentation

--- Backend/src/main/java/com/sena/urbantracker/config/SwaggerConfig.java
+++ Backend/src/main/java/com/sena/urbantracker/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.sena.urbantracker.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
-
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +15,12 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .security(List.of(new SecurityRequirement().addList("bearerAuth")))
                 .info(new Info()
                         .title("UrbanTracker API")
                         .version("1.0")
